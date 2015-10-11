cd1812e1caed47e13b0c87537556d4f1734d173c
상준 <guitarking91@naver.com>
2015-10-12 08:02:40 +0900

    ddd

--- calc/src/main/java/com/example/myapplication/MainActivity.java
+++ calc/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,52 +1,132 @@
 package com.example.myapplication;
 
+import android.app.Activity;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
 
-    @Override
+public class MainActivity extends Activity {
+    EditText edit1, edit2;
+    Button btnAdd, btnSub, btnMul, btnDiv, btnRest;
+    TextView textResult;
+    float result = 0;
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        edit1 = (EditText) findViewById(R.id.Edit1);
+        edit2 = (EditText) findViewById(R.id.Edit2);
+
+        btnAdd = (Button) findViewById(R.id.BtnAdd);
+        btnSub = (Button) findViewById(R.id.BtnSub);
+        btnMul = (Button) findViewById(R.id.BtnMul);
+        btnDiv = (Button) findViewById(R.id.BtnDiv);
+        btnRest = (Button) findViewById(R.id.BtnRest);
+
+        textResult = (TextView) findViewById(R.id.TextResult);
+
+        btnAdd.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+
+                String num1 = edit1.getText().toString();
+                String num2 = edit2.getText().toString();
+
+
+                if (num1.equals("") || num2.equals(""))
+                    Toast.makeText(MainActivity.this, "숫자를 입력하세요", Toast.LENGTH_SHORT).show();
+
+                else {
+
+                    float n1 = Float.parseFloat(num1);
+                    float n2 = Float.parseFloat(num2);
+                    result = n1 + n2;
+                    textResult.setText("계산 결과 : " + result);
+                }
+
+            }
+
+
+        });
+
+        btnSub.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String num1 = edit1.getText().toString();
+                String num2 = edit2.getText().toString();
+
+                if (num1.equals("") || num2.equals(""))
+                    Toast.makeText(MainActivity.this, "숫자를 입력하세요", Toast.LENGTH_SHORT).show();
+
+                else {
+
+                    float n1 = Float.parseFloat(num1);
+                    float n2 = Float.parseFloat(num2);
+                    result = n1 - n2;
+                    textResult.setText("계산 결과 : " + result);
+                }
             }
         });
-    }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
+        btnMul.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String num1 = edit1.getText().toString();
+                String num2 = edit2.getText().toString();
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+                if (num1.equals("") || num2.equals(""))
+                    Toast.makeText(MainActivity.this, "숫자를 입력하세요", Toast.LENGTH_SHORT).show();
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
+                else {
+                    float n1 = Float.parseFloat(num1);
+                    float n2 = Float.parseFloat(num2);
+                    result = n1 * n2;
+                    textResult.setText("계산 결과 : " + result);
+                }
+            }
+        });
 
-        return super.onOptionsItemSelected(item);
+        btnDiv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String num1 = edit1.getText().toString();
+                String num2 = edit2.getText().toString();
+
+                if (num1.equals("") || num2.equals(""))
+                    Toast.makeText(MainActivity.this, "숫자를 입력하세요", Toast.LENGTH_SHORT).show();
+                else if (num2.equals("0"))
+                    Toast.makeText(MainActivity.this, "0으로 나눌 수 없습니다", Toast.LENGTH_SHORT).show();
+
+                else {
+                    float n1 = Float.parseFloat(num1);
+                    float n2 = Float.parseFloat(num2);
+                    result = n1 / n2;
+                    textResult.setText("계산 결과 : " + result);
+                }
+            }
+        });
+
+        btnRest.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String num1 = edit1.getText().toString();
+                String num2 = edit2.getText().toString();
+
+                if (num1.equals("") || num2.equals(""))
+                    Toast.makeText(MainActivity.this, "숫자를 입력하세요", Toast.LENGTH_SHORT).show();
+
+                else {
+                    float n1 = Float.parseFloat(num1);
+                    float n2 = Float.parseFloat(num2);
+                    result = n1 % n2;
+                    textResult.setText("계산 결과 : " + result);
+                }
+            }
+        });
     }
 }
